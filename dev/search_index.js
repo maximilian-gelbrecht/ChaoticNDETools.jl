a2b751057343f3b198f97653bbde61d92f90655e
Documenter.jl <documenter@juliadocs.github.io>
2022-07-08 09:06:31 +0000

    build based on c4ccc84

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ChaoticNDETools","category":"page"},{"location":"#ChaoticNDETools","page":"Home","title":"ChaoticNDETools","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ChaoticNDETools.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ChaoticNDETools]","category":"page"}]
+}
