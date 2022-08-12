683c770e8df4128f868bcfa3080f6cf33e8e8bdc
Max <maximilian.gelbrecht@posteo.de>
2022-08-12 16:16:07 +0200

    added option for array input

--- src/tools.jl
+++ src/tools.jl
@@ -34,23 +34,23 @@ end
 
 Returns the average forecast length on a NODEDataloader set (should be valid or test set) given a `(t, u0) -> prediction` function. `N_t` is the length of each forecast, has to be larger than the expected forecast length. If a `λmax` is given, the results are scaled with it (and `dt``)
 """
-function average_forecast_length(predict, valid, N_t=300; λ_max=0, mode="norm")
+function average_forecast_length(predict, t::AbstractArray{T,1}, valid::AbstractArray{T,S}, N_t=300; λ_max=0, mode="norm") where {T,S}
 
     N = length(valid) - N_t
     @assert N >= 1 
-    
+
     forecasts = zeros(N)
-    dt = valid.t[2] - valid.t[1]
+    dt = t[2] - t[1]
     for i=1:N 
 
-        δ = forecast_δ(predict(valid.t[i:i+N_t], valid.data[..,i]), valid.data[..,i:i+N_t], mode)
+        δ = forecast_δ(predict(t[i:i+N_t], data[..,i]), data[..,i:i+N_t], mode)
         if λ_max == 0
             forecasts[i] = findall(δ .> 0.4)[1][2] 
         else 
             forecasts[i] = findall(δ .> 0.4)[1][2] * dt * λ_max
         end
-        GC.gc(true)
     end 
     return mean(forecasts)
 end
 
+average_forecast_length(predict, valid::NODEDataloader, N_t=300; kwargs...)  = average_forecast_length(predict, valid.t, valid.data, N_t; kwargs...) 
