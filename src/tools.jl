328d3efb63c003ec94e78361acb0ac438bd30265
Max <maximilian.gelbrecht@posteo.de>
2023-04-07 18:53:50 +0200

    update forecast lengths

--- src/tools.jl
+++ src/tools.jl
@@ -30,14 +30,20 @@ function forecast_δ(prediction::AbstractArray{T,N}, truth::AbstractArray{T,N}, 
 end
 
 """
-    forecast_lengths(model, valid::NODEDataloader,N_t=300; λmax=0, mode="norm")
+    forecast_lengths(model, t::AbstractArray{T,1}, input_data::AbstractArray{T,S}, N_t::Integer=300; λ_max=0, mode="norm", threshold=0.4, output_data::Union{Nothing, AbstractArray{T,S}}=nothing)
 
 Returns the forecast lengths of predictions on a NODEDataloader set (should be valid or test set) given a `(t, u0) -> prediction` function. `N_t` is the length of each forecast, has to be larger than the expected forecast length. If a `λmax` is given, the results are scaled with it (and `dt``)
 """
-function forecast_lengths(model, t::AbstractArray{T,1}, data::AbstractArray{T,S}, N_t::Integer=300; λ_max=0, mode="norm", threshold=0.4) where {T,S}
+function forecast_lengths(model, t::AbstractArray{T,1}, data::AbstractArray{T,S}, N_t::Integer=300; λ_max=0, mode="norm", threshold=0.4, output_data::Union{Nothing, AbstractArray{T,S}}=nothing) where {T,S}
 
     N = length(t) - N_t
     @assert N >= 1 
+
+    if isnothing(output_data)
+        output_data = data 
+    end
+
+    @assert size(data)[end] == size(output_data)[end]
 
     forecasts = zeros(N)
 
@@ -48,7 +54,7 @@ function forecast_lengths(model, t::AbstractArray{T,1}, data::AbstractArray{T,S}
     end
     
     for i=1:N 
-        δ = forecast_δ(model((t[i:i+N_t], data[..,i:i])), data[..,i:i+N_t], mode)
+        δ = forecast_δ(model((t[i:i+N_t], data[..,i:i])), output_data[..,i:i+N_t], mode)
         δ = δ[:] # return a 1x1...xN_t array, so we flatten it here
         first_ind = findfirst(δ .> threshold) 
 
